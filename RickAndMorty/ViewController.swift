3a731e0e34d41d94f9138ce194de92e2dc7dfff8
Fu-Syuan Wang <63849953+Tofuswang@users.noreply.github.com>
2023-10-23 21:21:42 +0800

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Terry Wang on 2023/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
