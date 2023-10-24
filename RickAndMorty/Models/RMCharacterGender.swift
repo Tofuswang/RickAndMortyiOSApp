ca71473753893968c2065ab9a701a4ff4807ff34
Fu-Syuan Wang <63849953+Tofuswang@users.noreply.github.com>
2023-10-24 23:42:46 +0800

    Updated the models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Terry Wang on 2023/10/24.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Terry Wang on 2023/10/24.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
